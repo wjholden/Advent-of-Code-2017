da55d104fc05b37f8de1245f4380afcd92362830
William John Holden <wjholden@gmail.com>
2025-01-19 18:33:27 +0100

    Day 11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,26 +1,66 @@
 fn main() {
     let puzzle = include_str!("../../puzzles/day11.txt").trim();
-    println!("Part 1: {}", part1(puzzle));
-    // println!("Part 2: {}", part2(puzzle));
+    let (part1, part2) = solve(puzzle);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
-fn part1(_input: &str) -> usize {
-    0
+fn solve(input: &str) -> (i32, i32) {
+    let mut distance = 0;
+    let mut x = 0;
+    let mut y = 0;
+    for step in input.split(",") {
+        match step {
+            "n" => y += 2,
+            "s" => y -= 2,
+            "nw" => {
+                x -= 1;
+                y += 1;
+            }
+            "sw" => {
+                x -= 1;
+                y -= 1;
+            }
+            "ne" => {
+                x += 1;
+                y += 1;
+            }
+            "se" => {
+                x += 1;
+                y -= 1;
+            }
+            _ => unreachable!(),
+        };
+        distance = distance.max(hex_distance(x, y));
+    }
+    (hex_distance(x, y), distance)
+}
+
+fn hex_distance(x: i32, y: i32) -> i32 {
+    x.abs() + (y.abs() - x.abs()) / 2
 }
 
 #[cfg(test)]
 mod day11 {
     use super::*;
 
-    const SAMPLE: &str = "";
-
     #[test]
     fn test1() {
-        assert_eq!(part1(SAMPLE), 0)
+        assert_eq!(solve("ne,ne,ne").0, 3)
     }
- 
+
     #[test]
     fn test2() {
-        //assert_eq!(part2(SAMPLE), 0)
-    }   
+        assert_eq!(solve("ne,ne,sw,sw").0, 0)
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(solve("ne,ne,s,s").0, 2)
+    }
+
+    #[test]
+    fn test4() {
+        assert_eq!(solve("se,sw,se,sw,sw").0, 3)
+    }
 }
